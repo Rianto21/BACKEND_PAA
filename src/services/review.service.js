a05b50cb1e7d37be634f42f749716d9d85e635e7
Svdex <ucihaibna@yahoo.com>
2022-05-26 03:25:29 +0700

    update get result

--- src/services/review.service.js
+++ src/services/review.service.js
@@ -2,12 +2,16 @@ const db = require("./db.service");
 
 async function getReview(product_id) {
     let res;
-    if (product_id !== undefined) {
-        let query = "SELECT * FROM reviews WHERE product_id = $1";
-        res = await db.executeQuery(query, [product_id]);
-    } else {
-        let query = "SELECT * FROM reviews";
-        res = await db.executeQuery(query);
+    try {
+        if (product_id !== undefined) {
+            let query = "SELECT * FROM reviews WHERE product_id = $1";
+            res = await db.executeQuery(query, [product_id]);
+        } else {
+            let query = "SELECT * FROM reviews";
+            res = await db.executeQuery(query);
+        }
+    } catch (err) {
+        throw Error(err.message);
     }
     return res;
 }
